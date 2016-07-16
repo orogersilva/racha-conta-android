c66e9f39b95f59701f717c76ac690b736dea81a6
Roger Silva <orogersilva@gmail.com>
2016-07-16 16:12:42 -0300

    Integrando Crashlytics.

--- app/src/main/java/com/orogersilva/rachaconta/taberna/MainActivity.java
+++ app/src/main/java/com/orogersilva/rachaconta/taberna/MainActivity.java
@@ -2,12 +2,17 @@ package com.orogersilva.rachaconta.taberna;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fabric.with(this, new Crashlytics());
     }
 }
